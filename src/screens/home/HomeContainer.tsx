aeca79b31cc7d4c2067e2abae4b992f0139a581a
Reddy <TReddy@nba.com>
2021-03-08 16:08:40 -0500

    Commit after eslint

--- src/screens/home/HomeContainer.tsx
+++ src/screens/home/HomeContainer.tsx
@@ -1,9 +1,11 @@
 function Home() {
-  return (
-    <div>
-      <div>test2</div>
-    </div>
-  );
+    const Tests123 = '1234';
+
+    return (
+        <div>
+            <div>test2 </div>
+        </div>
+    );
 }
 
 export default Home;
