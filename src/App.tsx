ffd23de6eb3aa72d6039573ead64841c40304a7d
Tirumala Reddy <rtirumala@gmail.com>
2021-03-12 21:08:13 -0500

    Add basic theme with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import { doc } from 'prettier';
+import React, { useState } from 'react';
 import './App.css';
 import Home from './screens/home/HomeContainer';
 
 function App(): JSX.Element {
+    const [bodyClass, setBodyClass] = useState('theme-dark');
+
     return (
-        <div className="container mx-auto px-4 bg-gray-100">
+        <div className={`container mx-auto px-4 bg-gray-100 {bodyClass}`}>
             <Home />
+            <div className="bg-primary">
+                <div>{bodyClass}</div>
+            </div>
+            <div className="flex">
+                <button
+                    className="flex-1 bg-gray-500"
+                    onClick={() => {
+                        setBodyClass('theme-dark');
+                        document.body.classList.remove('theme-light');
+                        document.body.classList.add('theme-dark');
+                    }}
+                >
+                    Dark Theme
+                </button>
+                <button
+                    className="flex-1 bg-gray-500"
+                    onClick={() => {
+                        setBodyClass('theme-light');
+                        document.body.classList.remove('theme-dark');
+                        document.body.classList.add('theme-light');
+                    }}
+                >
+                    Light Theme
+                </button>
+            </div>
         </div>
     );
 }
