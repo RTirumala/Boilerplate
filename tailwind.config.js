76596b1473cf1a18a78174ce09fe5f7552146d65
Reddy <TReddy@nba.com>
2021-03-13 19:18:49 -0500

    With prod compression

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,11 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px',
-        },
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-
-            black: colors.black,
-            white: colors.white,
-            gray: colors.coolGray,
-            red: colors.red,
-            yellow: colors.amber,
-            green: colors.emerald,
-            blue: colors.blue,
-            indigo: colors.indigo,
-            purple: colors.violet,
-            pink: colors.pink,
-            primary: 'var(--primary)',
-            secondary: 'var(--secondary)',
-        },
-        fontFamily: {
-            sans: ['Graphik', 'sans-serif'],
-            serif: ['Merriweather', 'serif'],
-        },
         extend: {
-            spacing: {
-                128: '32rem',
-                144: '36rem',
-            },
-            borderRadius: {
-                '4xl': '2rem',
+            colors: {
+                primary: 'var(--primary)',
+                secondary: 'var(--secondary)',
             },
         },
     },
