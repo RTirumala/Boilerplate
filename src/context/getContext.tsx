e971e2dbb141ca2d1090863f502c5839a0c716ce
RTirumala <rtirumala@gmail.com>
2021-03-17 17:56:11 -0400

    minor fix - git bash test

--- src/context/getContext.tsx
+++ src/context/getContext.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 type Props = {
-    title: string;
     children: React.ReactNode;
 };
 export function createCtx<StateType, ActionType>(
